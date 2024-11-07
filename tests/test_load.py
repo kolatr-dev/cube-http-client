5d93f13bdfb41109937055a1bdd9d5c3c4d9502e
Mal Curtis <mal@mal.co.nz>
2024-11-07 18:02:19 +1300

    Add test for continue_wait

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,5 +1,5 @@
 import pytest
-
+from pytest_mock import MockerFixture
 import cube_http
 from cube_http.types.v1.load import V1LoadRequestQueryDict
 
@@ -19,3 +19,17 @@ async def test_async_load(url: str, token: str, query: V1LoadRequestQueryDict):
     cube = cube_http.AsyncClient({"url": url, "token": token})
     result = await cube.v1.load(query)
     assert result is not None
+
+def test_load_continue_wait(url: str, token: str,  mocker: MockerFixture):
+    mock_response = mocker.MagicMock()
+    mock_response.status_code = 200
+    mock_response.json = mocker.MagicMock(side_effect=[{"error": "Continue wait"}, {"error": "Continue wait"}, {"results": []}])
+
+    mock_client = mocker.patch('httpx.Client')
+    mock_client.return_value.send.return_value = mock_response
+    mock_client.return_value.build_request.return_value = mocker.Mock()
+
+    cube = cube_http.Client({"url": url, "token": token})
+    cube.v1.load({})
+
+    assert mock_response.json.call_count == 3
